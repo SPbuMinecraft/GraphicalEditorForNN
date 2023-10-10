60128bac8da08f1e41c4b1ce24ab898814dc21a6
mavor <ma_voroshilov@mail.ru>
2023-10-11 01:02:24 +0300

    Rewrite predict and train methods. Add implementation of overall graph validator

--- py_server/app.py
+++ py_server/app.py
@@ -124,38 +124,45 @@ def train_model(user_id: int, model_id: int):
     allowed = sql_worker.verify_access(user_id, model_id)
     if not allowed:
         error(HTTPStatus.FORBIDDEN, "You have no rights for training this model")
-
+    json = request.json
+    if not json:
+        error(HTTPStatus.BAD_REQUEST, message="No json provided")
     try:
         model = sql_worker.get_graph_elements(model_id)
-        for i in range(len(model['layers'])):
-            model['layers'][i]['parameters'] = parse_parameters(model['layers'][i]['parameters'])
+        model['layers'] = list(map(lambda layer: parse_parameters(layer['parameters']), model['layers']))
         if not is_valid_model(model):
             error(HTTPStatus.NOT_ACCEPTABLE, "Invalid model found")
-        # response = requests.Response(text="OK", status_code=HTTPStatus.OK)
-        # requests.post(CPP_SERVER_ADDRESS + "/train", json=jsonify(model), timeout=3)  # Timeout=?
-        # What does 'train' method (C++) return?
-        return jsonify(model)  # return "Training finished", HTTPStatus.OK
+
+        # Convert json to another format for C++
+        model['connection'] = list(
+            map(lambda connection: [connection['layer_from'], connection['layer_to']], model['connection']))
+        model['dataset'] = json
+
+        response = requests.get(CPP_SERVER_ADDRESS + '/train', json=model, timeout=3)
+        return response.text, response.status_code
+    except KeyError as e:
+        error(HTTPStatus.BAD_REQUEST, str(e))
     except TimeoutError as e:
         error(HTTPStatus.REQUEST_TIMEOUT, "Training time limit exceeded")
-    # Observe possible errors and catch them
-    # except Exception as e:
-    #     error(HTTPStatus.BAD_REQUEST, message=str(e))
+    except Exception as e:
+        error(HTTPStatus.BAD_REQUEST, message=str(e))
 
 
-# dummy: TODO (in sprint 2)
-# Hardcoded function to make predictions for XOR model
 @app.route('/predict/<int:user_id>/<int:model_id>', methods=['POST'])
 def predict(user_id: int, model_id: int):
+    allowed = sql_worker.verify_access(user_id, model_id)
+    if not allowed:
+        error(HTTPStatus.FORBIDDEN, "You have no rights for training this model")
     json = request.json
     if not json:
         error(HTTPStatus.BAD_REQUEST, message="No json provided")
     try:
         response = requests.get(
             CPP_SERVER_ADDRESS + "/predict",
-            json=jsonify({"x": json["x"], "y": json["y"]}),
+            json=jsonify({"input": [[json["x"], json["y"]]]}),
             timeout=3
         )
-        return response.text, HTTPStatus.OK
+        return response.text, response.status_code
     except KeyError as e:
         error(HTTPStatus.BAD_REQUEST, str(e))
     except TimeoutError as e:
--- utils.py
+++ utils.py
@@ -28,8 +28,38 @@ def error(code: int, message: str):
     abort(Response(message, code))
 
 
+def get_edges_from_model(model_dict):
+    edges = dict()
+    for connection in model_dict['connections']:
+        layer_from, layer_to = connection['layer_from'], connection['layer_to']
+        if layer_from in edges.keys():
+            edges[layer_from].append(layer_to)
+        else:
+            edges[layer_from] = [layer_to]
+    return edges
+
+
 def is_valid_model(model_dict):
-    return True
+    edges = get_edges_from_model(model_dict)
+    start_candidates = list(filter(lambda x: x['type'] == 'Data', model_dict['layers']))
+    stop_candidates = list(filter(lambda x: x['type'] == 'Output', model_dict['layers']))
+    if len(start_candidates) != 1 or len(stop_candidates) != 1:
+        return False
+    start = start_candidates[0]['id']
+    stop = stop_candidates[0]['id']
+    # BFS realisation
+    distance = dict()
+    for layer in model_dict['layers']:
+        distance[layer['id']] = 0 if layer['id'] == start else -1
+    layers_queue = [start]
+    while layers_queue:
+        layer = layers_queue[0]
+        layers_queue.pop(0)
+        for layer_to in edges[layer]:
+            if distance[layer_to] == -1:
+                distance[layer_to] = distance[layer] + 1
+                layers_queue.append(layer_to)
+    return distance[stop] != -1
 
 
 def parse_parameters(layer_string: str) -> dict[str, tp.Any]:
