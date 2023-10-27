f87f2537c4c567184545cfbf1ddeefce95e82e01
Ivan Shanygin <88805084+AntoxaBarin@users.noreply.github.com>
2023-10-27 16:38:56 +0300

    ID-105: Fix layer parameters update (#33)

--- py_server/tests/test_update_layer.py
+++ py_server/tests/test_update_layer.py
@@ -14,18 +14,17 @@ def test_update_layer(client: FlaskClient, app: Flask):
     r = post_request(
         client,
         "/add_layer/1/1",
-        data={"type": "Linear", "parameters": {"inFeatures": 2, "outFeatures": 1}},
+        data={"type": "Linear", "parameters": "inFeatures=2;outFeatures=1"},
     )
     assert r.status_code == HTTPStatus.CREATED
     r = put_request(
         client,
         "/update_layer/1/1",
-        data={"id": 0, "parameters": {"inFeatures": 3, "outFeatures": 100}},
+        data={"id": 0, "parameters": "inFeatures=3;outFeatures=100"},
     )
     assert r.status_code == HTTPStatus.OK
     with app.app_context():
         m = db.session.get(Model, 1)
     assert m is not None
     params = json.loads(m.content)["layers"][0]["parameters"]
-    assert params["inFeatures"] == 3
-    assert params["outFeatures"] == 100
+    assert (params == "inFeatures=3;outFeatures=100" or params == "outFeatures=100;inFeatures=3")
