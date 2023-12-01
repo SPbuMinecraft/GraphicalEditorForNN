1ee67821aab09dbd143cc66c829b4ba4b96eb2e5
heartmarshall <heartmarshall@yandex.ru>
2023-12-01 14:53:55 +0300

    fix editor bug

--- client/scripts/auth.js
+++ client/scripts/auth.js
@@ -62,7 +62,6 @@ async function requestModel(user_id) {
 }
 
 async function loginUser() {
-    debugger
     console.log(loginForm)
     let errorArea = document
         .querySelector(".login-box")
