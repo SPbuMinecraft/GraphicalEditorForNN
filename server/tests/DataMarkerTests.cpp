30f5e25ac8f795082e0d9f1768959fd1f7dae865
MaxVorosh <ma_voroshilov@mail.ru>
2023-12-09 15:25:36 -0500

    Add batches

--- server/tests/DataMarkerTests.cpp
+++ server/tests/DataMarkerTests.cpp
@@ -16,7 +16,7 @@ void check_vectors(std::vector<std::pair<std::vector<float>, float>>& ans, std::
 
 TEST_CASE("Csv-test") {
     SUBCASE("and-train") {
-        DataMarker loader = DataMarker("./tests/data/and-train.csv", FileExtension::Csv, 50);
+        DataMarker loader = DataMarker("./tests/data/and-train.csv", FileExtension::Csv, 50, 1);
         DataLoader for_train = loader.get_train_loader();
         DataLoader for_check = loader.get_check_loader();
         std::vector<std::pair<std::vector<float>, float>> ans = {{{0, 0}, 0}, {{0, 1}, 0}, {{1, 0}, 0}, {{1, 1}, 1}};
@@ -24,14 +24,18 @@ TEST_CASE("Csv-test") {
         CHECK(for_train.size() == 2);
         CHECK(for_check.size() == 2);
         for (int i = 0; i < 2; ++i) {
-            res.push_back(for_train.get_raw(i));
-            res.push_back(for_check.get_raw(i));
+            auto line1 = for_train.get_raw(i);
+            CHECK(line1.second.size() == 1);
+            res.push_back({line1.first, line1.second[0]});
+            auto line2 = for_check.get_raw(i);
+            CHECK(line2.second.size() == 1);
+            res.push_back({line2.first, line2.second[0]});
         }
         sort(res.begin(), res.end());
         check_vectors(ans, res);
     }
     SUBCASE("xor-train") {
-        DataMarker loader = DataMarker("./tests/data/xor-train.csv", FileExtension::Csv, 50);
+        DataMarker loader = DataMarker("./tests/data/xor-train.csv", FileExtension::Csv, 50, 1);
         DataLoader for_train = loader.get_train_loader();
         DataLoader for_check = loader.get_check_loader();
         std::vector<std::pair<std::vector<float>, float>> ans = {{{0, 0}, 0}, {{0, 1}, 1}, {{1, 0}, 1}, {{1, 1}, 0}};
@@ -39,8 +43,12 @@ TEST_CASE("Csv-test") {
         CHECK(for_train.size() == 2);
         CHECK(for_check.size() == 2);
         for (int i = 0; i < 2; ++i) {
-            res.push_back(for_train.get_raw(i));
-            res.push_back(for_check.get_raw(i));
+            auto line1 = for_train.get_raw(i);
+            CHECK(line1.second.size() == 1);
+            res.push_back({line1.first, line1.second[0]});
+            auto line2 = for_check.get_raw(i);
+            CHECK(line2.second.size() == 1);
+            res.push_back({line2.first, line2.second[0]});
         }
         sort(res.begin(), res.end());
         check_vectors(ans, res);
@@ -48,7 +56,7 @@ TEST_CASE("Csv-test") {
 }
 
 TEST_CASE("Image-test") {
-    DataMarker loader = DataMarker("./tests/data/1", FileExtension::Png, 80);
+    DataMarker loader = DataMarker("./tests/data/1", FileExtension::Png, 80, 1);
     DataLoader for_train = loader.get_train_loader();
     DataLoader for_check = loader.get_check_loader();
     std::vector<std::pair<std::vector<float>, float>> ans = {{{255, 255, 255}, 0}, {{0, 0, 0}, 0}, {{159, 252, 253}, 0}, {{255, 255, 0, 0, 255, 255, 0, 0, 0}, 1}, {{0, 255, 100, 153, 136, 255, 0, 174, 100, 217, 0, 255, 0, 201, 100, 234, 21, 255}, 1}};
@@ -56,9 +64,12 @@ TEST_CASE("Image-test") {
     CHECK(for_train.size() == 4);
     CHECK(for_check.size() == 1);
     for (int i = 0; i < 4; ++i) {
-        res.push_back(for_train.get_raw(i));
+        auto line = for_train.get_raw(i);
+        CHECK(line.second.size() == 1);
+        res.push_back({line.first, line.second[0]});
     }
-    res.push_back(for_check.get_raw(0));
+    CHECK(for_check.get_raw(0).second.size() == 1);
+    res.push_back({for_check.get_raw(0).first, for_check.get_raw(0).second[0]});
     sort(res.begin(), res.end());
     sort(ans.begin(), ans.end());
     check_vectors(ans, res);
