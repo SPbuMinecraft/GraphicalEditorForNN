89ea4c6c7e27236acf2be9402bd516717b21b0bc
lpetrov02 <lpetrov02@mail.ru>
2023-12-23 15:16:57 +0300

    Adds lr scheduler Fixes scheduler Changes naming

--- server/core/Optimizer.h
+++ server/core/Optimizer.h
@@ -4,13 +4,35 @@
 
 #include "Tensor.h"
 
+class OptimizerBase;
+
+class LRScheduler {
+public:
+    virtual void step() = 0;
+    virtual ~LRScheduler() = default;
+};
+
+class GammaScheduler : public LRScheduler {
+public:
+    GammaScheduler(OptimizerBase* optim, size_t stepSize, float gamma);
+    virtual void step() override;
+
+private:
+    OptimizerBase* optim;
+    size_t stepSize;
+    float gamma;
+    size_t stepAccum = 0;
+};
+
 class OptimizerBase {
+    friend class GammaScheduler;
+    friend class ConstScheduler;
 public:
     OptimizerBase(float lr);
     void append(std::vector<TensorRef>& newParams);
     void step();
 
 private:
-    const float lr;
+    float lr;
     std::vector<TensorRef> params;
 };
